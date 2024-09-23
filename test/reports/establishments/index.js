00ff1b9f7384d944f6410444103aebd191635c9c
Jeffrey Horton <jeff.horton@digital.homeoffice.gov.uk>
2024-09-23 13:26:43 +0100

    Minor refactorings

--- test/reports/establishments/index.js
+++ test/reports/establishments/index.js
@@ -3,6 +3,38 @@ const sinon = require('sinon');
 
 const report = require('../../../lib/reports/establishments');
 
+let EXAMPLE_PELH = {
+  'id': '5b7bad13-f34b-4959-bd08-c6067ae2fcdd',
+  'migrated_id': null,
+  'user_id': '304cae96-0f56-492a-9f66-e99c2b3990c7',
+  'title': 'Dr',
+  'first_name': 'Bruce',
+  'last_name': 'Banner',
+  'dob': '1970-04-23',
+  'position': 'University Vice-Chancellor',
+  'qualifications': 'BLib',
+  'certifications': '',
+  'address': 'Lunar House\nSydenham Road,\nCroydon\nLondon',
+  'postcode': 'CR0 2YF',
+  'email': 'vice-chancellor@example.com',
+  'telephone': '01840 345 678',
+  'notes': '',
+  'created_at': '2024-09-23T08:54:03.644527+00:00',
+  'updated_at': '2024-09-23T08:54:03.644527+00:00',
+  'deleted': null,
+  'asru_user': false,
+  'asru_admin': false,
+  'asru_licensing': false,
+  'asru_inspector': false,
+  'asru_support': false,
+  'telephone_alt': null,
+  'pil_licence_number': null,
+  'rcvs_number': null,
+  'email_confirmed': true,
+  'last_login': '2024-09-23T10:07:40.312+00:00',
+  'asru_rops': false
+};
+
 const EXAMPLE_ESTABLISHMENT_DB_ROW = {
   'id': 8202,
   'name': 'Marvell Pharmaceutical',
@@ -17,68 +49,8 @@ const EXAMPLE_ESTABLISHMENT_DB_ROW = {
   'rehomes': true,
   'suitabilities': [['CAT', 'EQU'], ['AQ', 'EQU']],
   'pelh': [
-    {
-      'id': '5b7bad13-f34b-4959-bd08-c6067ae2fcdd',
-      'migrated_id': null,
-      'user_id': '304cae96-0f56-492a-9f66-e99c2b3990c7',
-      'title': 'Dr',
-      'first_name': 'Bruce',
-      'last_name': 'Banner',
-      'dob': '1970-04-23',
-      'position': 'University Vice-Chancellor',
-      'qualifications': 'BLib',
-      'certifications': '',
-      'address': 'Lunar House\nSydenham Road,\nCroydon\nLondon',
-      'postcode': 'CR0 2YF',
-      'email': 'vice-chancellor@example.com',
-      'telephone': '01840 345 678',
-      'notes': '',
-      'created_at': '2024-09-23T08:54:03.644527+00:00',
-      'updated_at': '2024-09-23T08:54:03.644527+00:00',
-      'deleted': null,
-      'asru_user': false,
-      'asru_admin': false,
-      'asru_licensing': false,
-      'asru_inspector': false,
-      'asru_support': false,
-      'telephone_alt': null,
-      'pil_licence_number': null,
-      'rcvs_number': null,
-      'email_confirmed': true,
-      'last_login': '2024-09-23T10:07:40.312+00:00',
-      'asru_rops': false
-    },
-    {
-      'id': '5b7bad13-f34b-4959-bd08-c6067ae2fcdd',
-      'migrated_id': null,
-      'user_id': '304cae96-0f56-492a-9f66-e99c2b3990c7',
-      'title': 'Dr',
-      'first_name': 'Bruce',
-      'last_name': 'Banner',
-      'dob': '1970-04-23',
-      'position': 'University Vice-Chancellor',
-      'qualifications': 'BLib',
-      'certifications': '',
-      'address': 'Lunar House\nSydenham Road,\nCroydon\nLondon',
-      'postcode': 'CR0 2YF',
-      'email': 'vice-chancellor@example.com',
-      'telephone': '01840 345 678',
-      'notes': '',
-      'created_at': '2024-09-23T08:54:03.644527+00:00',
-      'updated_at': '2024-09-23T08:54:03.644527+00:00',
-      'deleted': null,
-      'asru_user': false,
-      'asru_admin': false,
-      'asru_licensing': false,
-      'asru_inspector': false,
-      'asru_support': false,
-      'telephone_alt': null,
-      'pil_licence_number': null,
-      'rcvs_number': null,
-      'email_confirmed': true,
-      'last_login': '2024-09-23T10:07:40.312+00:00',
-      'asru_rops': false
-    }
+    EXAMPLE_PELH,
+    EXAMPLE_PELH
   ],
   'holc': [null, null],
   'asru': [null, null]
@@ -103,7 +75,7 @@ function buildDbMock() {
         throw new Error(`Unexpected status ${status}`);
     }
   });
-  // Inner builders are not checked as function name is enough which count is expected
+  // Inner builders are not checked as function name is enough to determine which count is expected
   mockBuilder.whereExists = sinon.fake(() => Promise.resolve([{count: 2}]));
   mockBuilder.whereNotExists = sinon.fake(() => Promise.resolve([{count: 3}]));
 
