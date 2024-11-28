e303da4e0b6a7b3bce92be83ff84867ff505e164
farrukhishaq <farrukh.ishaq@homeoffice.gov.uk>
2024-11-28 12:02:09 +0000

    corrected knex initialising - will need to be repointed for migration file.

--- scripts/asl-migrate.js
+++ scripts/asl-migrate.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const path = require('path');
 
 async function getConfig() {
-  const { test, development } = await import('@asl/schema/knexfile');
+  const { test, development } = await import('@asl/schema/knexfile.js');
   let env = null;
   if (process.env.NODE_ENV === 'test') {
     env = test;
@@ -17,19 +17,19 @@ async function getConfig() {
  * @return void - run latest migration.
  * */
 async function runMigrations() {
-  const env = getConfig();
-  knex(env);
+  const env = await getConfig();
+  const knexInstance = await knex(env);
   try {
     console.log('Running migrations...');
-    await knex.migrate.latest({
-      directory: path.resolve(__dirname, '../../node_modules/@asl/schema/migrations') // Ensure migration dir points to dependency
+    await knexInstance.migrate.latest({
+      directory: path.resolve(__dirname, '../../../node_modules/@asl/schema/migrations')
     });
     console.log('Migrations completed.');
   } catch (error) {
     console.error('Error running migrations:', error);
   } finally {
-    await knex.destroy();
+    await knexInstance.destroy();
   }
 }
-
+runMigrations();
 module.exports = runMigrations;
